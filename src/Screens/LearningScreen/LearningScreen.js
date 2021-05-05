bd1a75bacadf8e8eeeb9160937dd2b467c1b065c
student <daniel.her@onja.org>
2021-05-05 09:50:25 +0300

    Display all phrases

--- src/Screens/LearningScreen/LearningScreen.js
+++ src/Screens/LearningScreen/LearningScreen.js
@@ -1,40 +1,107 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Text, SafeAreaView} from 'react-native';
+import {useDispatch} from 'react-redux';
 import {utilities} from '../../utils/utils';
 
+// Components
 import PhraseTextarea from '../../components/PhraseTextarea/PhraseTextArea';
 import SectionHeading from '../../components/SectionHeading/SectionHeading';
 import List from '../../components/List/List';
 import ListItem from '../../components/ListItem/ListItem';
+
+// Buttons
 import ActionButton from '../../components/ActionButton/ActionButton';
 import NextButton from '../../components/NextButton/NextButton';
 import SwitcherButton from '../../components/SwitcherButton/SwitcherButton';
 import ToolButton from '../../components/ToolButton/ToolButton';
-
 import ScreenModeIcon from '../../icons/screen-mode-icon.svg';
 import BackIcon from '../../icons/left-arrow.svg';
-import {useDispatch} from 'react-redux';
 
 function LearningScreen({route, navigation}) {
   const {
+    phrasesList,
     languageOption,
     selectedLanguage,
     translatorLanguage,
     setSelectedLanguage,
     setTranslatorLanguage,
-    answers,
     switcher,
     switchLanguage,
-    learningLogics,
+    isDisabled,
+    setIsDisabled,
   } = utilities();
+
+  const [randomPhrase, setRandomPhrase] = useState('');
+  const [correctAnswer, setCorrectAnswer] = useState('');
+
+  const [answers, setAnswers] = useState('');
 
   const dispatch = useDispatch();
   const [headingText, setHeadingText] = useState('Category:');
   const [subHeading, setSubHeading] = useState('The phrase:');
   const [testHeading, setTestHeading] = useState('Pick a solution:');
   const [buttonName, setButtonName] = useState('Next');
-
   const optionItem = route.params.item;
+
+  // Get random numbers
+  let randomIndex1 = Math.floor(Math.random() * optionItem.phrasesIds.length);
+  let randomIndex2 = Math.floor(Math.random() * optionItem.phrasesIds.length);
+  let randomIndex3 = Math.floor(Math.random() * optionItem.phrasesIds.length);
+  let randomIndex4 = Math.floor(Math.random() * optionItem.phrasesIds.length);
+
+  // Get 4 random phrases
+  const phrase1 = phrasesList.find(
+    phrase => phrase.id === optionItem.phrasesIds[randomIndex1],
+  );
+  const phrase2 = phrasesList.find(
+    phrase => phrase.id === optionItem.phrasesIds[randomIndex2],
+  );
+  const phrase3 = phrasesList.find(
+    phrase => phrase.id === optionItem.phrasesIds[randomIndex3],
+  );
+  const phrase4 = phrasesList.find(
+    phrase => phrase.id === optionItem.phrasesIds[randomIndex4],
+  );
+  console.log(answers, randomPhrase.answer);
+  function learningLogics() {
+    const answersArray = [
+      {
+        answer: phrase1.name,
+        phraseId: phrase1.id,
+        id: 1,
+      },
+      {
+        answer: phrase2.name,
+        phraseId: phrase2.id,
+        id: 2,
+      },
+      {
+        answer: phrase3.name,
+        phraseId: phrase3.id,
+        id: 3,
+      },
+      {
+        answer: phrase4.name,
+        phraseId: phrase4.id,
+        id: 4,
+      },
+    ];
+
+    const randomizingAnswer = answersArray.sort(
+      (a, b) => a.answer.mg.length - b.answer.en.length,
+    );
+    setAnswers(randomizingAnswer);
+    setCorrectAnswer({
+      answer: phrase1.name,
+      id: phrase1.id,
+    });
+    setIsDisabled(false);
+    setRandomPhrase({answer: phrase1.name, id: phrase1.id});
+  }
+
+  useEffect(() => {
+    learningLogics();
+  }, []);
 
   function changeLanguage() {
     if (switcher) {
@@ -54,6 +121,12 @@ function LearningScreen({route, navigation}) {
     }
     dispatch(switchLanguage(true));
   }
+
+  function pickAnAnswer({text}) {
+    console.log(text);
+    setIsDisabled(true);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={[styles.button, styles.row]}>
@@ -85,7 +158,7 @@ function LearningScreen({route, navigation}) {
       <View style={{marginBottom: 37}}>
         <SectionHeading headingText={subHeading} />
         <PhraseTextarea
-          // phrase={randomPhrase.name[selectedLanguage]}
+          phrase={randomPhrase && randomPhrase.answer[translatorLanguage]}
           editable={false}
         />
       </View>
@@ -93,12 +166,19 @@ function LearningScreen({route, navigation}) {
         <List
           headingText={testHeading}
           data={answers}
-          renderItem={({item}) => (
-            <ListItem
-              text={item.answer[selectedLanguage]}
-              buttonText={switcher ? 'Haka' : 'Pick'}
-            />
-          )}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => {
+            return (
+              <ListItem
+                disabled={isDisabled}
+                key={item.id}
+                id={item.id}
+                text={item.answer[selectedLanguage]}
+                buttonText={switcher ? 'Haka' : 'Pick'}
+                handlePress={pickAnAnswer}
+              />
+            );
+          }}
         />
       </View>
       <NextButton buttonText={buttonName} handleNext={learningLogics} />
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -10,81 +10,24 @@ export const utilities = () => {
   const categoriesList = categoriesData.categories;
   const phrasesList = phrasesData.phrases;
   const [selectedLanguage, setSelectedLanguage] = useState(languageOption.EN);
+  const [isDisabled, setIsDisabled] = useState(false);
   const [translatorLanguage, setTranslatorLanguage] = useState(
     LANGUAGE_NAMES.MG,
   );
 
   const switcher = useSelector(state => state.switchLanguage);
 
-  const [randomPhrase, setRandomPhrase] = useState('');
-  const [correctAnswer, setCorrectAnswer] = useState({});
-
-  //   const phrasesList = phrasesData.phrases.filter(
-  //     (phrase, index) => phrase.id === optionItem.phrasesIds[index],
-  //   );
-
-  // const phraseToDisplay = phrasesData.phrases.find(
-  //   phrase => phrase.id === randomPhrase.id,
-  // );
-
-  const [answers, setAnswers] = useState('');
-
-  // Get random numbers
-  const randomIndex1 = Math.floor(Math.random() * categoriesList.length);
-  const randomIndex2 = Math.floor(Math.random() * categoriesList.length);
-  const randomIndex3 = Math.floor(Math.random() * categoriesList.length);
-  const randomIndex4 = Math.floor(Math.random() * categoriesList.length);
-
-  // Created 4 answers to choose
-
-  const checkId1 = phrasesData.phrases.filter(
-    item => item.id === '###introductions###',
-  );
-
-  console.log(correctAnswer.id, checkId1);
-
-  function learningLogics() {
-    setAnswers([
-      {
-        answer: categoriesList[randomIndex1].name,
-        id: categoriesList[randomIndex1].id,
-      },
-      {
-        answer: categoriesList[randomIndex2].name,
-        id: categoriesList[randomIndex2].id,
-      },
-      {
-        answer: categoriesList[randomIndex3].name,
-        id: categoriesList[randomIndex3].id,
-      },
-      {
-        answer: categoriesList[randomIndex4].name,
-        id: categoriesList[randomIndex4].id,
-      },
-    ]);
-    setCorrectAnswer({
-      answer: categoriesList[randomIndex1].name,
-      id: categoriesList[randomIndex1].id,
-    });
-    const randomCategory = categoriesList[randomIndex1].phrasesIds;
-    setRandomPhrase(randomCategory);
-  }
-
-  useEffect(() => {
-    learningLogics();
-  }, []);
-
   return {
     selectedLanguage,
     setSelectedLanguage,
     setTranslatorLanguage,
     translatorLanguage,
-    randomPhrase,
-    correctAnswer,
-    answers,
     switcher,
     switchLanguage,
-    learningLogics,
+    categoriesList,
+    phrasesList,
     languageOption,
+    isDisabled,
+    setIsDisabled,
   };
 };
